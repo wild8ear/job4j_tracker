f6f83ca4e6611f86671fcc6deb51f69920fdb8fd
wild8ear <wild8ear@yandex.ru>
2023-03-17 23:46:20 +0300

    3. Массивы и модели. [#174892]

---
+++ src/main/java/ru/job4j/pojo/Book.java
@@ -0,0 +1,19 @@
+package ru.job4j.pojo;
+
+public class Book {
+    private String name;
+    private int countOfPages;
+
+    public Book(String name, int countOfPages) {
+        this.name = name;
+        this.countOfPages = countOfPages;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCountOfPages() {
+        return countOfPages;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -0,0 +1,31 @@
+package ru.job4j.pojo;
+
+public class Library {
+    public static void main(String[] args) {
+        Book book1 = new Book("Clean code", 200);
+        Book book2 = new Book("Dirty code", 199);
+        Book book3 = new Book("Amazing code", 1000);
+        Book book4 = new Book("Mad code", 10);
+        Book[] books = new Book[4];
+        books[0] = book1;
+        books[1] = book2;
+        books[2] = book3;
+        books[3] = book4;
+        for (Book book: books) {
+            System.out.println("The book name is: " + book.getName()
+                + " which has " + book.getCountOfPages() + " pages");
+        }
+        books[3] = book1;
+        books[0] = book4;
+        for (Book book: books) {
+            System.out.println("The book name is: " + book.getName()
+                    + " which has " + book.getCountOfPages() + " pages");
+        }
+        for (Book book: books) {
+            if (book.getName().equals("Clean code")) {
+                System.out.println("The book name is: " + book.getName()
+                        + " which has " + book.getCountOfPages() + " pages");
+            }
+        }
+    }
+}
