258598c54075f29832d1a18b244a0760c0b9cbae
wild8ear <wild8ear@yandex.ru>
2023-03-18 14:55:05 +0300

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -20,6 +22,32 @@ public class Tracker {
                 break;
             }
         }
+        return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int i = 0;
+        for (int index = 0; index < size; index++) {
+            if (key.equals(items[index].getName())) {
+                rsl[i] = items[index];
+                i++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, i);
+        return  rsl;
+    }
+
+    public Item[] findAll() {
+        Item[] rsl = new Item[size];
+        int i = 0;
+        for (int index = 0; index < size; index++) {
+            if (items[index].getName() != null) {
+                rsl[i] = items[index];
+                i++;
+            }
+        }
+        rsl = Arrays.copyOf(rsl, i);
         return rsl;
     }
 }
