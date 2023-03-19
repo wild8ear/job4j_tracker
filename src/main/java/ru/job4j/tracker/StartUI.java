db86dc9690caba071b16209ffa0b0e3f02bdae55
wild8ear <wild8ear@yandex.ru>
2023-03-19 22:24:37 +0300

    10. toString [#310018]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,9 +8,7 @@ public class StartUI {
 
     public static void main(String[] args) {
         Item item = new Item();
-        LocalDateTime localDateTime = item.getCreated();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-EEEE-yyyy HH:mm:ss");
-        System.out.println(localDateTime.format(formatter));
+        System.out.println(item);
 
     }
 }
