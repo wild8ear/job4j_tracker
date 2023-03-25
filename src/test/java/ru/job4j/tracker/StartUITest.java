d5c3d4155080db7331bdc85ef935ba72e4bb6384
wild8ear <wild8ear@yandex.ru>
2023-03-25 14:34:36 +0300

    12.0. Написать тесты на StartUI. [#785 #404115]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -45,5 +45,6 @@ public class StartUITest {
                 new DeleteAction(),
                 new ExitAction()
         };
+        assertThat(tracker.findById(item.getId())).isNull();
     }
 }
