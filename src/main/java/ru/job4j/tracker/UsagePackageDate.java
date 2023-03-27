c4748ceaf1638b861f83d3ff4a2e37fb1bbe92c5
wild8ear <wild8ear@yandex.ru>
2023-03-27 07:32:28 +0300

    12.1 Зависимость от System.out [#33568]

--- src/main/java/ru/job4j/tracker/UsagePackageDate.java
+++ src/main/java/ru/job4j/tracker/UsagePackageDate.java
@@ -4,12 +4,14 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class UsagePackageDate {
+
     public static void main(String[] args) {
+        Output out = new ConsoleOutput();
         LocalDateTime currentDateTime = LocalDateTime.now();
-        System.out.println("Текущие дата и время: " + currentDateTime);
+        out.println("Текущие дата и время: " + currentDateTime);
 
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MMMM-yyyy HH:mm:ss");
         String currentDateTimeFormat = currentDateTime.format(formatter);
-        System.out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
+        out.println("Текущие дата и время после форматирования: " + currentDateTimeFormat);
     }
 }
--- src/main/java/ru/job4j/tracker/UserStore.java
+++ src/main/java/ru/job4j/tracker/UserStore.java
@@ -3,7 +3,8 @@ package ru.job4j.tracker;
 public class UserStore {
 
     public static void main(String[] args) {
+        Output out = new ConsoleOutput();
         User user = new User(1, "root", "root");
-        System.out.println(user);
+        out.println(user);
     }
 }
