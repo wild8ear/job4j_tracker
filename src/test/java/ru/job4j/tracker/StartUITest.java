a25c0fb7da85093b5fdc25a8a90a48f37f218de9
wild8ear <wild8ear@yandex.ru>
2023-03-27 07:07:41 +0300

    12.0. Написать тесты на StartUI. [#785 #404115]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -25,7 +25,7 @@ public class StartUITest {
         Item item = tracker.add(new Item("Replaced item"));
         String replaceName = "New item name";
         Input in = new StubInput(
-                new String[] {"0", "1", "New item name", "1"}
+                new String[] {"0", String.valueOf(item.getId()), replaceName, "1"}
         );
         UserAction[] actions = {
                 new EditAction(),
@@ -40,7 +40,7 @@ public class StartUITest {
         Tracker tracker = new Tracker();
         Item item = tracker.add(new Item("Delete item"));
         Input in = new StubInput(
-                new String[] {"0", "1", "1"}
+                new String[] {"0", String.valueOf(item.getId()), "1"}
         );
         UserAction[] actions = {
                 new DeleteAction(),
