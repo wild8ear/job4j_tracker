604c15936b37d7daee5ea821a369d094156e0914
wild8ear <wild8ear@yandex.ru>
2023-03-31 11:06:10 +0300

    0.4. Пользовательские исключения. [#219367 #405738]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -2,7 +2,6 @@ package ru.job4j.ex;
 
 public class FindEl {
     public static int indexOf(String[] value, String key) throws ElementNotFoundException {
-        int rsl = -1;
         for (int index = 0; index < value.length; index++) {
             if (key.equals(value[index])) {
                 return index;
