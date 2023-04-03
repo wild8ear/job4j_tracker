3c691fcd6bac607f3d4dd80d9778fa6d40c62784
wild8ear <wild8ear@yandex.ru>
2023-04-03 10:36:09 +0300

    2.1. Тест на ValidateInput [#311887 #406392]

--- src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -17,7 +17,7 @@ public class ValidateInputTest {
     }
 
     @Test
-    public void whenValidInnput() {
+    public void whenValidInput() {
         Output out = new StubOutput();
         Input in = new StubInput(new String[] {"1"}
         );
@@ -27,20 +27,21 @@ public class ValidateInputTest {
     }
 
     @Test
-    public void whenMultyValidInnput() {
+    public void whenMultyValidInput() {
         Output out = new StubOutput();
         String[] commands = new String[] {"0", "1", "2"};
         Input in = new StubInput(commands);
-        for (String select : commands) {
-            ValidateInput input = new ValidateInput(out, in);
-            int selected = input.askInt("Enter menu:");
-            assertThat(selected).isEqualTo(Integer.parseInt(select));
-        }
-
+        ValidateInput input = new ValidateInput(out, in);
+        int selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(0);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(1);
+        selected = input.askInt("Enter menu:");
+        assertThat(selected).isEqualTo(2);
     }
 
     @Test
-    public void whenNegativeNumberInnput() {
+    public void whenNegativeNumberInput() {
         Output out = new StubOutput();
         Input in = new StubInput(new String[] {"-1"}
         );
