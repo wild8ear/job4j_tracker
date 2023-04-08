80a4b9a54e037c3a6bddb9fc8dee1d55bf211cb7
wild8ear <wild8ear@yandex.ru>
2023-04-08 21:04:10 +0300

    1. Singleton [#94606 #407641]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -2,9 +2,16 @@ package ru.job4j.tracker;
 
 public final class SingleTracker {
 
-    private static Tracker tracker = new Tracker();
+    private static Tracker tracker = null;
 
     private SingleTracker() { }
+
+    public static Tracker getInstance() {
+        if (tracker == null) {
+            tracker = new Tracker();
+        }
+        return tracker;
+    }
 
     public Item add(Item item) {
         return tracker.add(item);
