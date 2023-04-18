cacabc0917dcdc013b874705881dd322f5933673
wild8ear <wild8ear@yandex.ru>
2023-04-18 21:23:51 +0300

    2. Коллекции, ArrayList, List, Обобщения [#10094 #410431]

--- src/main/java/ru/job4j/collection/UsageArrayList.java
+++ src/main/java/ru/job4j/collection/UsageArrayList.java
@@ -8,7 +8,7 @@ public class UsageArrayList {
         names.add("Petr");
         names.add("Ivan");
         names.add("Stepan");
-        for (String name: names) {
+        for (String name : names) {
             System.out.println(name);
         }
     }
