0c49d594e2011cecc20b8df686a0d6ed8905fb53
wild8ear <wild8ear@yandex.ru>
2023-05-12 07:47:58 +0300

    5. Фильтрация учеников [#110058 #415710]

--- src/test/java/ru/job4j/stream/SchoolTest.java
+++ src/test/java/ru/job4j/stream/SchoolTest.java
@@ -37,7 +37,7 @@ public class SchoolTest {
                 new Student(80, "Surname8")
         );
         School school = new School();
-        Predicate<Student> predicate = student -> student.getScore() >= 50 && student.getScore() <= 70;
+        Predicate<Student> predicate = student -> student.getScore() > 50 && student.getScore() < 70;
         List<Student> result = school.collect(students, predicate);
         List<Student> expected = new ArrayList<>();
         expected.add(new Student(50, "Surname5"));
