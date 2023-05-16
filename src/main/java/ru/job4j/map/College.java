eb60764b681a0788ac16d1b805696fdb659e4a4d
wild8ear <wild8ear@yandex.ru>
2023-05-17 00:13:22 +0300

    3. Optional - empty, of [#214711 #416851]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import java.util.Set;
 
 public class College {
-
     private final Map<Student, Set<Subject>> students;
 
     public College(Map<Student, Set<Subject>> students) {
