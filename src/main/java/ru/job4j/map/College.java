4b640df74f8f69037c018b846aa7609ca1eb614d
wild8ear <wild8ear@yandex.ru>
2023-05-17 00:10:07 +0300

    3. Optional - empty, of [#214711]

--- src/main/java/ru/job4j/map/College.java
+++ src/main/java/ru/job4j/map/College.java
@@ -1,34 +1,39 @@
 package ru.job4j.map;
 
+import java.util.Optional;
 import java.util.Map;
 import java.util.Set;
 
 public class College {
-
     private final Map<Student, Set<Subject>> students;
 
     public College(Map<Student, Set<Subject>> students) {
         this.students = students;
     }
 
-    public Student findByAccount(String account) {
-        return students.keySet()
-                .stream()
-                .filter(s -> s.account().equals(account))
-                .findFirst()
-                .orElse(null);
+    public Optional<Student> findByAccount(String account) {
+        Optional<Student> rsl = Optional.empty();
+        for (Student student : students.keySet()) {
+            if (student.account().equals(account)) {
+                rsl = Optional.of(student);
+                break;
+            }
+        }
+        return rsl;
     }
 
-    public Subject findBySubjectName(String account, String name) {
-        Student a = findByAccount(account);
-        if (a != null) {
-            return students.get(a)
-                    .stream()
-                    .filter(s -> s.name().equals(name))
-                    .findFirst()
-                    .orElse(null);
+    public Optional<Subject> findBySubjectName(String account, String name) {
+        Optional<Subject> subjectOptional = Optional.empty();
+        Optional<Student> studentOptional = findByAccount(account);
+        if (studentOptional.isPresent()) {
+            for (Subject sub : students.get(studentOptional.get())) {
+                if (sub.name().equals(name)) {
+                    subjectOptional = Optional.of(sub);
+                    break;
+                }
+            }
         }
-        return null;
+        return subjectOptional;
     }
 
     public static void main(String[] args) {
@@ -39,9 +44,9 @@ public class College {
                 )
         );
         College college = new College(students);
-        Student student = college.findByAccount("000001");
-        System.out.println("Найденный студент: " + student);
-        Subject english = college.findBySubjectName("000001", "English");
-        System.out.println("Оценка по найденному предмету: " + english.score());
+        Optional<Student> student = college.findByAccount("000001");
+        System.out.println("Найденный студент: " + student.get());
+        Optional<Subject> english = college.findBySubjectName("000001", "English");
+        System.out.println("Оценка по найденному предмету: " + english.get().score());
     }
 }
