78b9caadf7ef3ec07f4efdf4b2149b71cf4e0eb2
wild8ear <wild8ear@yandex.ru>
2023-05-28 19:34:46 +0300

    3. Аттестация [#49777 #418813]

--- src/main/java/ru/job4j/stream/Analyze.java
+++ src/main/java/ru/job4j/stream/Analyze.java
@@ -33,7 +33,7 @@ public class Analyze {
         return stream.map(Pupil -> new Tuple(Pupil.name(), Pupil.subjects().stream()
                         .mapToInt(Subject::score)
                         .sum())).max(Comparator.comparingDouble(Tuple::score))
-                .orElse(new Tuple("", 0D));
+                .orElse(null);
     }
 
     public static Tuple bestSubject(Stream<Pupil> stream) {
@@ -42,6 +42,6 @@ public class Analyze {
                         Collectors.summingDouble(Subject::score))).entrySet().stream()
                 .map(item -> new Tuple(item.getKey(), item.getValue()))
                 .max(Comparator.comparingDouble(Tuple::score))
-                .orElse(new Tuple("", 0D));
+                .orElse(null);
     }
 }
